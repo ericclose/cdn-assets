50b01b3d5f1985f12708275f09f48e798f8d4f22
ericclose <ZetaGo@protonmail.com>
2020-05-07 20:19:13 +0800

    add spoilers

--- js/custom.js
+++ js/custom.js
@@ -19,3 +19,8 @@ $( window ).resize( function () {
   });
 // Resize to fix all iframes on page load.
 }).resize();
+
+// spoilers
+$(document).on('click', '.hide', function() {
+  $(this).removeClass('hide');
+});
